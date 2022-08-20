ecd866ab0df492559f1901410db9993e8b863ccf
loloof64 <laurent.bernabe@gmail.com>
2022-08-20 12:09:59 +0200

    Added license file

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -26,7 +26,7 @@ fun App() {
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(onCloseRequest = ::exitApplication, title = "Basic chess endgames") {
         App()
     }
 }
